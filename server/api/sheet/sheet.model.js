d2edd33a701357d9086771b82eabb829b49f627c
Houssam HANED <houssam@hhmedia.fr>
2016-09-19 15:59:13 +0200

    Ajout de la route vers answerSheet

--- server/api/sheet/sheet.model.js
+++ server/api/sheet/sheet.model.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 import mongoose from 'mongoose';
 
 var SheetSchema = new mongoose.Schema({
--- server/config/seed.js
+++ server/config/seed.js
@@ -5,6 +5,7 @@
 
 'use strict';
 import User from '../api/user/user.model';
+import Sheet from '../api/sheet/sheet.model';
 
 User.find({}).remove()
   .then(() => {
@@ -22,5 +23,51 @@ User.find({}).remove()
     })
     .then(() => {
       console.log('finished populating users');
+
+      User.findOne({'email':'test@example.com'}, (err,user) => {
+        createSheetSeed(user._id);
+      });
+
     });
   });
+
+
+
+function createSheetSeed(userId) {
+
+  Sheet.find({}).remove()
+    .then(() => {
+      Sheet.create({
+        name: 'Test sur github pour le cesi',
+        creator: userId,
+        type : 'Checkbox',
+        questions: [{
+          name: 'Quel est la commande pour récupérer un repo distant',
+          order : 1,
+          answers: [
+            {value: 'commit', right: false},
+            {value: 'push',   right: false},
+            {value: 'clone',  right: true},
+            {value: 'fetch',  right: false}
+          ]
+        }],
+        forUser : [
+          {email:'houssam.haned@viacesi.fr'},
+          {email:'houssam.haned2@viacesi.fr'},
+          {email:'houssam.haned3@viacesi.fr'},
+          {email:'houssam.haned4@viacesi.fr'},
+        ],
+        active: true,
+        private: true,
+        lock : true,
+      })
+        .then(() => {
+          console.log('finished populating sheet');
+        });
+    });
+
+
+}
+
+
+
