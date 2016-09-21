70a4d380b09ab0e148daf74c3f519a2c419e736f
Houssam HANED <houssam@hhmedia.fr>
2016-09-21 11:53:19 +0200

    Change home text

--- client/assets/js/init.js
+++ client/assets/js/init.js
@@ -1,8 +1,6 @@
 (function($){
   $(function(){
-    // var test = jQuery('.button-collapse');
-    // console.log( jQuery($('.button-collapse')).sideNav());
-    // test.sideNav();
-    $('.button-collapse').sideNav('show');
+    $('.button-collapse').sideNav();
   });
 })(jQuery);
+
